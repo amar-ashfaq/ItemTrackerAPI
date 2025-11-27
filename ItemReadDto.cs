09aed5ef8874a54e5747c3f02761f11ea2f3aed2
Amar AA <amar.ashfaq95@gmail.com>
2025-11-27 18:38:29 +0000

    Created the ItemDtos' - one for Create/Update and one for Read/Response.

--- ItemReadDto.cs
+++ ItemReadDto.cs
@@ -1,7 +1,8 @@
 ﻿namespace ItemTrackerAPI
 {
-    public class ItemDTO
+    public class ItemReadDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
